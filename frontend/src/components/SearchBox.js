0d0a0f534eab51a3adebaf96c52435abb8d50394
Marta Niewiadomska <martaen@gmail.com>
2022-01-31 15:07:50 +0100

    mobile displaying

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -15,7 +15,7 @@ const SearchBox = ({ history }) => {
   }
 
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} inline autocomplete="off">
       <Form.Control
         type='text'
         name='q'
