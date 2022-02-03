4ba2b8e54e774c79e71fb21abe83e5a26b5ef77d
Marta Niewiadomska <martaen@gmail.com>
2022-02-03 10:04:45 +0100

    cache

--- backend/middleware/cacheMiddleware.js
+++ backend/middleware/cacheMiddleware.js
@@ -1,10 +1,10 @@
 const setCache =  (req, res, next) => {
     // here you can define period in second, this one is 5 minutes
-    const period = 60 * 5 
+   
   
     // you only want to cache for GET requests
     if (req.method == 'GET') {
-      res.set('Cache-control', `public, max-age=${period}`)
+      res.set('Cache-control', `no-cache, max-age=0, private`)
     } else {
       // for the other requests set strict no caching parameters
       res.set('Cache-control', `no-store`)
